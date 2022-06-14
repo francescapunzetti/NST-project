d80520c8d7315e2d7ffcbf4242cfa172d4735a8f
Francesca <93604148+francescapunzetti@users.noreply.github.com>
2022-06-14 18:46:48 +0200

    1.1
    Using PIL to convert in image, but still doesn't work

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -1,52 +1,73 @@
 # import the necessary packages
 import argparse
-import imutils
+#import imutils
 import time
+from tkinter import Y
 import cv2
+import numpy as np
 from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
+from keras import Model
+import matplotlib.pyplot as plt
+import keras
+import os 
+from keras.applications.resnet import preprocess_input
+from keras.applications.resnet import decode_predictions
+from keras.preprocessing import image
+from tensorflow.python.keras.models import Model
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+#python neural_style_transfer.py --image /Users/francescapunzetti/Desktop/2/fre.jpg \   
+#        --model /Users/francescapunzetti/Desktop/2/the_wave.t7
+import IPython.display as display
 
-Image.MAX_IMAGE_PIXELS = None
-# construct the argument parser and parse the arguments
-#questi commenti servono nel momento tu non sappia che sono model and image
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True,
-	help="neural style transfer model") #neural style transfer model path
-ap.add_argument("-i", "--image", required=True,
-	help="input image to apply neural style transfer to") #image output
-args = vars(ap.parse_args())
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+import h5py
+import numpy as np
+import PIL.Image
+from PIL import Image as im
+import time
+import os
+import tensorflow as tf
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
 
-# load the neural style transfer model from disk
-print("[INFO] loading style transfer model...")
-net = cv2.dnn.readNetFromTorch(args["model"])
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
 
-# load the input image, resize it to have a width of 600 pixels, and
-# then grab the image dimensions
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=600)
-(h, w) = image.shape[:2]
-# construct a blob from the image, set the input, and then perform a
-# forward pass of the network
-blob = cv2.dnn.blobFromImage(image, 1.0, (w, h),
-	(103.939, 116.779, 123.680), swapRB=False, crop=False)
-net.setInput(blob)
-start = time.time()
-output = net.forward()
-end = time.time()
+from keras.models import load_model
+import cv2
+import numpy as np
 
-# reshape the output tensor, add back in the mean subtraction, and
-# then swap the channel ordering
-output = output.reshape((3, output.shape[2], output.shape[3]))
-output[0] += 103.939
-output[1] += 116.779
-output[2] += 123.680
-output /= 255.0
-output = output.transpose(1, 2, 0)
+model = load_model('best_model.h5')
 
-# show information on how long inference took
-print("[INFO] neural style transfer took {:.4f} seconds".format(
-	end - start))
+model.compile(optimizer='adam',
+              metrics=['accuracy'])
 
-# show the images
-cv2.imshow("Input", image)
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+img = cv2.imread('fre.jpg')
+img = cv2.resize(img,(320,240))
+img = np.reshape(img,[1,320,240,3])
+
+classes = model.predict(img, batch_size =1)
+print(classes.shape)
+
+data= tensor_to_image(classes)
+#data = im.fromarray(classes)
+#if data.mode != 'RGB':
+    #data = data.convert('L')
+data.save('transformed.jpg')
+
+
+
+
+
+#m = h5py.File('best_model.h5', 'r')
